695ac00533fd42cdf774b82f73ec1280ba8c19b9
tanishk11777 <tanishkgoyal38@gmail.com>
2024-06-21 12:43:53 +0530

    add  all

---
+++ lib/GptHome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GptHome extends StatefulWidget {
+  const GptHome({super.key});
+
+  @override
+  State<GptHome> createState() => _GptHomeState();
+}
+
+class _GptHomeState extends State<GptHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
